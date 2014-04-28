ac3d9693f118643fa6f36e673865872c2ded408b
Katrina Owen <katrina.owen@gmail.com>
2014-04-28 14:05:41 -0400

    Fix version of minitest in bottles

--- bottles/test/bottles_test.rb
+++ bottles/test/bottles_test.rb
@@ -1,9 +1,9 @@
-gem 'minitest', '~> 4.2'
+gem 'minitest', '~> 5.3'
 require 'minitest/autorun'
 require 'minitest/pride'
 require_relative 'bottles'
 
-class BottlesTest < MiniTest::Unit::TestCase
+class BottlesTest < Minitest::Test
 
   attr_reader :bottles
   def setup
